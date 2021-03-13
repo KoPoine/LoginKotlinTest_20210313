967f15a97bae94783db57759342f2bc8b359edf7
NePPPlus_010 <kmc901206@gmail.com>
2021-03-13 11:29:29 +0900

    입력한 아이디 비번을 추출까지

--- app/src/main/java/com/example/loginkotlintest_20210313/MainActivity.kt
+++ app/src/main/java/com/example/loginkotlintest_20210313/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        버튼 클릭 이벤트
+
+
+
+//        그안에서
+//        1. 아이디 / 비번에 적힌 값 => 변수2개로 저장
+
+        val inputId = idEdt.text.toString()
+        val inputPw = pwEdt.text.toString()
+
+//        2. 두 변수를 갖고 조건문 활용
+
+
     }
 }
