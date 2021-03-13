2a02a7c52080b78eb06f35002f1a342fc28113e2
NePPPlus_010 <kmc901206@gmail.com>
2021-03-13 11:33:43 +0900

    조건문 부분 완성   - == (같은가?), && (양쪽이 다 맞나?)   -  toast 자동완성

--- app/src/main/java/com/example/loginkotlintest_20210313/MainActivity.kt
+++ app/src/main/java/com/example/loginkotlintest_20210313/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.loginkotlintest_20210313
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,15 +12,27 @@ class MainActivity : AppCompatActivity() {
 
 //        버튼 클릭 이벤트
 
+        loginBtn.setOnClickListener {
 
 
 //        그안에서
 //        1. 아이디 / 비번에 적힌 값 => 변수2개로 저장
 
-        val inputId = idEdt.text.toString()
-        val inputPw = pwEdt.text.toString()
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
 
 //        2. 두 변수를 갖고 조건문 활용
+
+            if(inputId == "admin" && inputPw == "qwer") {
+
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
 
 
     }
